55162b9274c8454cca50c29febbb67e71f0d7a77
maaguaete <tigermaaguaete@gmail.com>
2021-12-10 00:37:19 +0700

    update membership

---
+++ main.js
@@ -0,0 +1,17 @@
+ // When the user scrolls down 300px from the top of the document, resize the header's font size
+ //  $(document).ready(function() {
+ //      if ($(window).width() > 992) {
+
+ //          $(window).scroll(function() {
+ //              if ($(window).scrollTop() > 70) {
+ //                  $("header").css("height", "90px");
+ //                  $("header").children().css("height", "90px");
+ //                  $(".logo").css("opacity", "0");
+ //              } else {
+ //                  $("header").css("height", "246px");
+ //                  $("header").children().css("height", "100%");
+ //                  $(".logo").css("opacity", "1");
+ //              }
+ //          });
+ //      }
+ //  });
